4fa5115b2f1554f761fe2146bd51d7055349d8a9
André Nogueira <estevo.andr@gmail.com>
2021-03-30 08:43:33 -0300

    WIP: Working on db functions   - Added change-object-case, to convert javascript camelCase to     postgresql snake_case       - Added some type declarations on main.d.ts   - Refactored some old code to use await instead of multiple promise     chains   - Removed query function (will make select, update and others later)   - Added select function     - This uses a function to generate the "where" section of the query       dinamically       - Got stuck in trying to get a value from an object using a dynamic       value as the property, didn't figure it out yet

---
+++ main.d.ts
@@ -0,0 +1,5 @@
+declare module "change-object-case"{
+  export type changeableCase = Object | String;
+  export function camelCase<changeableCase>(arg: changeableCase): changeableCase;
+  export function snakeCase<changeableCase>(arg: changeableCase): changeableCase;
+};
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
-import pg, { Pool } from "pg";
+import pg, { Pool, QueryResult } from "pg";
 import dotenv from "dotenv";
 import path from "path";
+import { camelCase, snakeCase } from "change-object-case";
+import { getProperty } from "../utils";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
@@ -11,41 +13,54 @@ const pool = new Pool({
 });
 
 // checking connection health
-pool
-.connect()
-.then((client: pg.PoolClient) => {
-  return client
-  .query("SELECT now()")
-  .then((res) => {
+pool.connect().then(async (client: pg.PoolClient) => {
+  try {
+    const response = await client.query("SELECT now()");
+    console.log("Connection to database OK:", response.rows[0].now);
+  } catch (err) {
+    console.error(err.stack);
+    process.exit();
+  } finally {
     client.release();
-    console.log(res.rows[0]);
-  })
-  .catch((err) => {
-    client.release();
-    console.log(err.stack);
-  });
-})
-.catch((connectionError) => {
-  console.error(connectionError);
-  process.exit();
+  }
 });
 
-const query = async (queryString: string, queryArguments: string[]) => {
-  return await pool.connect().then((client: pg.PoolClient) => {
-    return client
-      .query(queryString, queryArguments)
-      .then((res) => {
-        client.release();
-        return [true, res.rows];
-      })
-      .catch((err) => {
-        client.release();
-        console.log(err.stack);
-        return [false, err.stack];
-      });
-  });
+const select = async (
+  table: String,
+  whereParams: Object
+): Promise<QueryResult | false> => {
+  const query = `SELECT * FROM ${table} WHERE ${generateWhere(whereParams)}`;
+
+  const client = await pool.connect();
+  try {
+    return client.query(query);
+  } catch (error) {
+    console.log(error);
+    return false;
+  } finally {
+    client.release();
+  }
 };
 
-export default { 
-  query
+const generateWhere = (whereParams: Object): String => {
+  whereParams = snakeCase(whereParams);
+  const whereString = Object.keys(whereParams).reduce(
+    (accumulator: string, currentValue: string, index: number): string => {
+      if (index > 0) {
+        accumulator += ", ";
+      }
+
+      accumulator += `${currentValue} = ${getProperty(
+        whereParams,
+        currentValue
+      )}`;
+
+      return accumulator;
+    },
+    ""
+  );
+
+  return whereString;
 };
+
+export { select };
